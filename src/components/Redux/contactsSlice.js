22e6abbf7410724d4858d7a2fad2d1d15af0c672
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-09-20 13:58:52 +0200

    start

---
+++ src/components/Redux/contactsSlice.js
@@ -0,0 +1,73 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/contacts'); // Вам потрібно замінити цей URL на свій бекенд URL
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contactData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('/contacts', contactData); // Вам потрібно замінити цей URL на свій бекенд URL
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, { rejectWithValue }) => {
+    try {
+      await axios.delete(`/contacts/${contactId}`); // Вам потрібно замінити цей URL на свій бекенд URL
+      return contactId;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.items = state.items.filter((contact) => contact.id !== action.payload);
+      });
+  },
+});
+
+export default contactsSlice.reducer;
